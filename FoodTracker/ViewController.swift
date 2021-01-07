8cf3d48011cfdcb4914bc9b1a9e4a36011e20b84
urvib3 <urvibhuwania25@gmail.com>
2021-01-07 15:48:48 -0800

    Lesson2

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     //MARK: Properties
     
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +33,52 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
 
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard
+        nameTextField.resignFirstResponder()
+        
+        // initialize our imagePickerController
+        let imagePickerController = UIImagePickerController()
+        
+        // make our image picker controller full screen
+        imagePickerController.modalPresentationStyle = .fullScreen
+        
+        // photos can only be accessed from photo library
+        imagePickerController.sourceType = .photoLibrary
+        
+        // make sure that view controller gets a notification whenever an image is selected
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        // dismiss the image picker controller when user cancels
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // Xcode 11: UIImagePickerControllerOriginalImage
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        else {
+            fatalError("Expected an image, got the following \(info)")
+        }
+        
+        // set photo image view to selected image
+        photoImageView.image = selectedImage
+        
+        // dismiss image picker controller after image is selected
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
